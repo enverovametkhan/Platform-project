af42e954e0fd2f51e65506341d93a1cf3195620b
enverovametkhan <enverov.ametkhan@gmail.com>
2023-09-01 19:14:56 +0600

    totally added path and hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import React, { useEffect } from "react";
 import LandingPage from "./LandingPage/LandingPage";
 import BlogButtons from "src/components/BlogButtons/BlogButtons";
 import ImageGallery from "src/components/ImageGallery/ImageGallery";
@@ -9,18 +8,24 @@ import Footer from "src/components/Footer/Footer";
 
 const router = createBrowserRouter([{ path: "/", element: <LandingPage /> }]);
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <AuthButtons />
-        <LandingPage />
-        <BlogButtons />
-        <ImageGallery />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  useEffect(() => {
+    console.log("App component mounted");
+
+    return () => {
+      console.log("App component unmounted");
+    };
+  }, []);
+
+  return (
+    <div className="app">
+      <AuthButtons />
+      <LandingPage />
+      <BlogButtons />
+      <ImageGallery />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
