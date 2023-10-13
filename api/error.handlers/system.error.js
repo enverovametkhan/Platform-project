ba9e6d184ec2b720ca96ad92b7abe5077d659321
enverovametkhan <enverov.ametkhan@gmail.com>
2023-10-13 08:46:24 +0600

    fixed system errors

--- api/error.handlers/system.error.js
+++ api/error.handlers/system.error.js
@@ -1,10 +1,12 @@
 process.on("uncaughtException", (error) => {
+  console.error("Dealing with Unhandled Exception in a unique way");
   console.error("Uncaught Exception:", error);
 
   process.exit(1);
 });
 
 process.on("unhandledRejection", (reason, promise) => {
+  console.error("Dealing with Unhandled Rejection in a unique way");
   console.error("Unhandled Rejection:", reason);
 
   process.exit(1);
