6f585a00d1dc89bb44b14052b2b79b50d3a2e816
enverovametkhan <enverov.ametkhan@gmail.com>
2023-11-23 19:47:36 +0600

    system error mezmo

--- api/error.handlers/system.error.js
+++ api/error.handlers/system.error.js
@@ -1,13 +1,20 @@
+const { customLogger } = require("../pack/mezmo");
+
 process.on("uncaughtException", (error) => {
-  console.error("Dealing with Unhandled Exception in a unique way");
-  console.error("Uncaught Exception:", error);
+  customLogger.consoleError("Server shutting down", {
+    message: "Dealing with Unhandled Exception in a unique way",
+    error: error.stack,
+  });
 
   process.exit(1);
 });
 
 process.on("unhandledRejection", (reason, promise) => {
-  console.error("Dealing with Unhandled Rejection in a unique way");
-  console.error("Unhandled Rejection:", reason);
+  customLogger.consoleError("Server shutting down", {
+    message: "Dealing with Unhandled Rejection in a unique way",
+    error: reason.stack,
+    reason: reason,
+  });
 
   process.exit(1);
 });
