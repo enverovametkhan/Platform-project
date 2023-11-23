544ec9abc066c58e0177c4709c228257b888bb43
enverovametkhan <enverov.ametkhan@gmail.com>
2023-11-23 19:40:22 +0600

    exception filter mezmo

--- api/error.handlers/exception.filter.js
+++ api/error.handlers/exception.filter.js
@@ -1,3 +1,5 @@
+const { customLogger } = require("../pack/mezmo");
+
 module.exports = (app) => {
   app.use((err, req, res, next) => {
     console.error("Error Handling Middleware");
@@ -6,6 +8,16 @@ module.exports = (app) => {
 
     const timeTaken = (Date.now() - req.timeStarted) / 1000;
     console.log(`Time taken to process the request: ${timeTaken} seconds`);
+
+    customLogger.consoleMiddleware(
+      `[EXCEPTION FILTER] - (ERROR) | Error caught in Exception Filter: ${req.originalUrl}`,
+      {
+        timeTaken: `${timeTaken} seconds`,
+        originURL: req.originalUrl,
+        method: req.method,
+        error: err,
+      }
+    );
 
     res
       .status(500)
