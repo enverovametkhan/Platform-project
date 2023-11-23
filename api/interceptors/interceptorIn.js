0b90a979e8639fabe01ea97abad549b726f60118
enverovametkhan <enverov.ametkhan@gmail.com>
2023-11-23 19:37:47 +0600

    interceptor out mezmo

--- api/interceptors/interceptorIn.js
+++ api/interceptors/interceptorIn.js
@@ -1,16 +1,5 @@
 const { customLogger } = require("../pack/mezmo");
 
-// module.exports = (app) => {
-//   app.use((req, res, next) => {
-//     customLogger.consoleMiddleware()
-//     res.setHeader("x-custom-header", Date.now());
-
-//     console.log(`Incoming Request: ${req.originalUrl}`);
-//     req.timeStarted = Date.now();
-//     next();
-//   });
-// };
-
 module.exports = (app) => {
   app.use((req, res, next) => {
     customLogger.consoleMiddleware(
@@ -19,9 +8,7 @@ module.exports = (app) => {
         origin: req.headers.origin,
         requestedURL: req.originalUrl,
         method: req.method,
-        ip: req.headers["cf-connecting-ip"],
-      },
-      "IN"
+      }
     );
 
     req.timeStarted = Date.now();
--- api/interceptors/interceptorOut.js
+++ api/interceptors/interceptorOut.js
@@ -1,9 +1,20 @@
+const { customLogger } = require("../pack/mezmo");
+
 module.exports = (app) => {
   app.use((req, res) => {
     console.log(`About to send response for ${req.originalUrl}`);
     const timeTaken = (Date.now() - req.timeStarted) / 1000;
     console.log(
       `Time taken to process the request on ${req.originalUrl} is ${timeTaken} seconds`
+    );
+
+    customLogger.consoleMiddleware(
+      `[INTERCEPTOR] - (OUT) | Outgoing Response: ${req.originalUrl}`,
+      {
+        timeTaken: `${timeTaken} seconds`,
+        originURL: req.originalUrl,
+        method: req.method,
+      }
     );
 
     let response = {
