7c71d933006bd048fe0e907b74d6030ee1d7aec4
enverovametkhan <enverov.ametkhan@gmail.com>
2023-11-23 19:31:48 +0600

    interceptor in mezmo

--- api/interceptors/interceptorIn.js
+++ api/interceptors/interceptorIn.js
@@ -1,8 +1,29 @@
+const { customLogger } = require("../pack/mezmo");
+
+// module.exports = (app) => {
+//   app.use((req, res, next) => {
+//     customLogger.consoleMiddleware()
+//     res.setHeader("x-custom-header", Date.now());
+
+//     console.log(`Incoming Request: ${req.originalUrl}`);
+//     req.timeStarted = Date.now();
+//     next();
+//   });
+// };
+
 module.exports = (app) => {
   app.use((req, res, next) => {
-    res.setHeader("x-custom-header", Date.now());
+    customLogger.consoleMiddleware(
+      `[INTERCEPTOR] - (IN) | Incoming Request: ${req.originalUrl}`,
+      {
+        origin: req.headers.origin,
+        requestedURL: req.originalUrl,
+        method: req.method,
+        ip: req.headers["cf-connecting-ip"],
+      },
+      "IN"
+    );
 
-    console.log(`Incoming Request: ${req.originalUrl}`);
     req.timeStarted = Date.now();
     next();
   });
