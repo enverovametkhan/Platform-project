2be5c9a6ad98a0d1936e1fc1177ebe6f7139a8b5
enverovametkhan <enverov.ametkhan@gmail.com>
2023-12-08 19:25:18 +0600

    refresh access token testing

--- api/tests/authTests/refreshAccessToken.test.js
+++ api/tests/authTests/refreshAccessToken.test.js
@@ -1,74 +1,114 @@
-// const chai = require("chai");
-// const sinon = require("sinon");
-// const chaiHttp = require("chai-http");
-// const { expect } = chai;
-// const { app } = require("../../app");
-// const { UserModel } = require("../../users/users.data");
-// const { createToken, decryptToken } = require("../../utilities/jwt");
+const chai = require("chai");
+const sinon = require("sinon");
+const chaiHttp = require("chai-http");
+const { expect } = chai;
+const { app } = require("../../app");
+const { UserModel } = require("../../users/users.data");
+const { createToken, decryptToken } = require("../../utilities/jwt");
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-// describe("REFRESH ACCESS TOKEN", () => {
-//   let findByIdStub, saveStub;
+describe("REFRESH ACCESS TOKEN", () => {
+  let findByIdStub, saveStub;
 
-//   beforeEach(() => {
-//     findByIdStub = sinon.stub(UserModel, "findById");
-//     saveStub = sinon.stub(UserModel.prototype, "save");
-//   });
+  beforeEach(() => {
+    findByIdStub = sinon.stub(UserModel, "findById");
+    saveStub = sinon.stub(UserModel.prototype, "save");
+  });
 
-//   afterEach(() => {
-//     findByIdStub.restore();
-//     saveStub.restore();
-//   });
+  afterEach(() => {
+    findByIdStub.restore();
+    saveStub.restore();
+  });
 
-//   it(`should refresh access token successfully`, async () => {
-//     const userDataJwt = {
-//       userId: "655c92eebe63597606646e1f",
-//       email: "1@11",
-//       username: "user12sd22f11",
-//     };
-//     const user = {
-//       _id: userDataJwt.userId,
-//       username: userDataJwt.username,
-//       email: userDataJwt.email,
-//     };
-//     const userId = "655c92eebe63597606646e1f";
-//     const userData = {
-//       userId,
-//       email: "1@11",
-//       username: "user12sd22f11",
-//     };
-//     findByIdStub.resolves(user);
-//     const token = await createToken(userData, "7d");
-//     const accessJwtToken = await createToken(userDataJwt, "300h");
+  it(`should refresh access token successfully`, async () => {
+    const userDataJwt = {
+      userId: "655c92eebe63597606646e1f",
+      email: "1@11",
+      username: "user12sd22f11",
+    };
+    const user = {
+      _id: userDataJwt.userId,
+      username: userDataJwt.username,
+      email: userDataJwt.email,
+    };
+    const token = await createToken(userDataJwt, "7d");
 
-//     const response = await chai
-//       .request(app)
-//       .set("Authorization", `Bearer ${accessJwtToken}`)
-//       .get(`/api/user/refreshAccessToken/${token}`);
+    findByIdStub.resolves(user);
 
-//     expect(response).to.have.status(200);
-//   });
+    const response = await chai
+      .request(app)
+      .get(`/api/user/refreshAccessToken/${token}`)
+      .set("Authorization", `Bearer ${token}`);
 
-//   it(`should return an error if user is not found during token refresh`, async () => {
-//     const token = await createToken({ userId: "nonexistentUserId" }, "1h");
+    expect(response).to.have.status(200);
+  });
 
-//     findByIdStub.withArgs("nonexistentUserId").resolves(null);
+  it(`should return an error if user is not found`, async () => {
+    const userDataJwt = {
+      userId: "655c92eebe63597606646e1f",
+      email: "1@11",
+      username: "user12sd22f11",
+    };
+    const token = await createToken(userDataJwt, "7d");
 
-//     const response = await chai
-//       .request(app)
-//       .get(`/api/user/refreshAccessToken/${token}`);
+    findByIdStub.resolves(null);
 
-//     expect(response).to.have.status(500);
-//   });
+    const response = await chai
+      .request(app)
+      .get(`/api/user/refreshAccessToken/${token}`)
+      .set("Authorization", `Bearer ${token}`);
 
-//   it(`should return an error if token is invalid during token refresh`, async () => {
-//     const token = "invalidToken";
+    expect(response).to.have.status(500);
+  });
 
-//     const response = await chai
-//       .request(app)
-//       .get(`/api/user/refreshAccessToken/${token}`);
+  it("should return an error if access token is expired", async () => {
+    const userDataJwt = {
+      userId: "655c92eebe63597606646e1f",
+      email: "1@11",
+      username: "user12sd22f11",
+    };
+    const accessJwtToken = await createToken(userDataJwt, "-1s");
 
-//     expect(response).to.have.status(500);
-//   });
-// });
+    const response = await chai
+      .request(app)
+      .get(`/api/user/refreshAccessToken/${accessJwtToken}`)
+      .set("Authorization", `Bearer ${accessJwtToken}`);
+
+    expect(response).to.have.status(500);
+  });
+
+  it("should return an error if refresh token is expired", async () => {
+    const userDataJwt = {
+      userId: "655c92eebe63597606646e1f",
+      email: "1@11",
+      username: "user12sd22f11",
+    };
+    const refreshJwtToken = await createToken(userDataJwt, "-1s");
+
+    const response = await chai
+      .request(app)
+      .get(`/api/user/refreshAccessToken/${refreshJwtToken}`)
+      .set("Authorization", `Bearer ${refreshJwtToken}`);
+
+    expect(response).to.have.status(500);
+  });
+
+  it("should return an error if both tokens are expired", async () => {
+    const userDataJwt = {
+      userId: "655c92eebe63597606646e1f",
+      email: "1@11",
+      username: "user12sd22f11",
+    };
+    const accessJwtToken = await createToken(userDataJwt, "-1s");
+    const refreshJwtToken = await createToken(userDataJwt, "-1s");
+
+    const response = await chai
+      .request(app)
+      .get(`/api/user/refreshAccessToken/${accessJwtToken}`)
+      .set("Authorization", `Bearer ${accessJwtToken}`)
+      .set("Refresh-Token", refreshJwtToken);
+
+    expect(response).to.have.status(500);
+  });
+});
