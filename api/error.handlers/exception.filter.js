480734b1826b1b24cff2ac21c9c1f106369c8e01
enverovametkhan <enverov.ametkhan@gmail.com>
2024-01-02 17:00:23 +0600

    exception filter

--- api/error.handlers/exception.filter.js
+++ api/error.handlers/exception.filter.js
@@ -1,26 +1,70 @@
-const { customLogger } = require("../pack/mezmo");
+// const { customLogger } = require("../pack/mezmo");
+
+// module.exports = (app) => {
+//   app.use((err, req, res, next) => {
+//     console.error("Error Handling Middleware");
+//     console.error("Error handling");
+//     console.error(err);
+
+//     const timeTaken = (Date.now() - req.timeStarted) / 1000;
+//     console.log(`Time taken to process the request: ${timeTaken} seconds`);
+
+//     customLogger.consoleError(
+//       `[EXCEPTION FILTER] - (ERROR) | Error caught in Exception Filter: ${req.originalUrl}`,
+//       {
+//         timeTaken: `${timeTaken} seconds`,
+//         originURL: req.originalUrl,
+//         method: req.method,
+//         error: err,
+//       }
+//     );
+
+//     res
+//       .status(500)
+//       .send({ message: err.errorMessage || "Internal Server Error" });
+//   });
+// };
 
 module.exports = (app) => {
   app.use((err, req, res, next) => {
-    console.error("Error Handling Middleware");
-    console.error("Error handling");
-    console.error(err);
+    try {
+      const timeTaken = (Date.now() - req.timeStarted) / 1000;
 
-    const timeTaken = (Date.now() - req.timeStarted) / 1000;
-    console.log(`Time taken to process the request: ${timeTaken} seconds`);
+      const sensitiveErrors = [
+        "Invalid Signature",
+        "Invalid JWT",
+        "jwt expired",
+      ];
 
-    customLogger.consoleError(
-      `[EXCEPTION FILTER] - (ERROR) | Error caught in Exception Filter: ${req.originalUrl}`,
-      {
-        timeTaken: `${timeTaken} seconds`,
-        originURL: req.originalUrl,
-        method: req.method,
-        error: err,
-      }
-    );
+      const processErrorMessage = (originalErrorMessage) =>
+        sensitiveErrors.some((error) => originalErrorMessage.includes(error))
+          ? "Internal Server Error"
+          : originalErrorMessage;
 
-    res
-      .status(500)
-      .send({ message: err.errorMessage || "Internal Server Error" });
+      let proccessedErrorMessage = processErrorMessage(
+        err.originalErrorMessage
+      );
+
+      customLogger.consoleError(
+        "[EXCEPTION FILTER] (OUT) | Oops something went wrong, sending an Exception Filter Error",
+        {
+          message: processErrorMessage,
+          timeTaken: `Time taken to process the request: ${timeTaken} seconds`,
+          requestedUrl: req.originalUrl,
+          origin: req.headers.origin,
+          ip: req.headers["cf-connecting-ip"],
+          method: req.method,
+          // ...err,
+        }
+      );
+      // custom error message needed when Invalid unauth token
+      // yet to find out when i need original Error Message
+
+      res
+        .status(500)
+        .send({ message: proccessedErrorMessage || "Internal Server Error" });
+    } catch (e) {
+      res.status(500).send({ message: "Internal Server Error" });
+    }
   });
 };
