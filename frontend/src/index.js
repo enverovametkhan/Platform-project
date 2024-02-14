c36c15e77f4df679452bcd2b9a55663b07883ea1
enverovametkhan <enverov.ametkhan@gmail.com>
2024-02-14 11:03:12 +0600

    url

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,7 +6,7 @@ import "./index.css";
 import App from "src/App";
 
 const root = createRoot(document.getElementById("root"));
-console.log(process.env.REACT_APP_BASE_URL);
+
 root.render(
   <Provider store={store}>
     <App />
