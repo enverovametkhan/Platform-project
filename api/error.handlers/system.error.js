3454f5abb748adaeae3a56add3a459f2b29a2acf
enverovametkhan <enverov.ametkhan@gmail.com>
2024-05-08 22:02:54 +0500

    system

--- api/error.handlers/system.error.js
+++ api/error.handlers/system.error.js
@@ -8,6 +8,12 @@ process.on("uncaughtException", (error) => {
   process.exit(1);
 });
 
+process.on("unhandledRejection", (reason, promise) => {
+  customLogger.consoleError("Server shutting down", {
+    message: "Dealing with Unhandled Rejection in a unique way",
+    error: reason.stack,
+    reason: reason,
+  });
 
   process.exit(1);
 });
